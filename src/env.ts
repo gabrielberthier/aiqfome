05adbb0fc844306111e474e44d5fe5c704ca753a
Gabriel <gabriel.nsilva@yduqs.com.br>
2025-06-21 03:07:35 -0300

    fix: remove unused var

--- src/env.ts
+++ src/env.ts
@@ -16,7 +16,6 @@ const EnvSchema = z.object({
   PORT: z.coerce.number().default(8000),
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"]).default("info"),
   DATABASE_URL: z.string().url().default("postgres://postgres:password@localhost:5432/aiqfome"),
-  DATABASE_AUTH_TOKEN: z.string().optional(),
   DB_MIGRATING: z.coerce.boolean(),
   DB_SEEDING: z.coerce.boolean(),
   BETTER_AUTH_SECRET: z.coerce.string().optional(),
@@ -25,15 +24,15 @@ const EnvSchema = z.object({
   REDIS_HOST: z.coerce.string().optional().default("localhost"),
   REDIS_PORT: z.coerce.string().optional().default("6379"),
 }).superRefine((input, ctx) => {
-  if (input.NODE_ENV === "production" && !input.DATABASE_AUTH_TOKEN) {
-    ctx.addIssue({
-      code: z.ZodIssueCode.invalid_type,
-      expected: "string",
-      received: "undefined",
-      path: ["DATABASE_AUTH_TOKEN"],
-      message: "Must be set when NODE_ENV is 'production'",
-    });
-  }
+  // if (input.NODE_ENV === "production" && !input.DATABASE_AUTH_TOKEN) {
+  //   ctx.addIssue({
+  //     code: z.ZodIssueCode.invalid_type,
+  //     expected: "string",
+  //     received: "undefined",
+  //     path: ["DATABASE_AUTH_TOKEN"],
+  //     message: "Must be set when NODE_ENV is 'production'",
+  //   });
+  // }
 });
 
 export type Env = z.infer<typeof EnvSchema>;
